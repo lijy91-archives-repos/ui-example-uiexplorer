f05e1bfa6f94c26a13a770f18da9f0f21f7d0fe4
JianyingLi <lijy91@foxmail.com>
2018-09-08 20:43:00 +0800

    firt commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import { AppRegistry, Platform } from 'react-native';
+import iconFont from 'react-native-vector-icons/Fonts/MaterialIcons.ttf';
+import App from './src/App';
+
+AppRegistry.registerComponent('UIExplorer', () => App);
+if (Platform.OS === 'web') {
+  const iconFontStyles = `@font-face {
+    src: url(${iconFont});
+    font-family: 'MaterialIcons';
+  }`;
+
+  // Create stylesheet
+  const style = document.createElement('style');
+  style.type = 'text/css';
+  if (style.styleSheet) {
+    style.styleSheet.cssText = iconFontStyles;
+  } else {
+    style.appendChild(document.createTextNode(iconFontStyles));
+  }
+
+  // Inject stylesheet
+  document.head.appendChild(style);
+  AppRegistry.runApplication('UIExplorer', {
+    rootTag: document.getElementById('root'),
+  });
+}
---
+++ src/navigators/AppNavigator.js
@@ -0,0 +1,12 @@
+// import { Platform } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+// import BrowserAppContainer from './BrowserAppContainer';
+
+import routeConfigMap from './routeConfigMap';
+import stackConfig from './stackConfig';
+
+const DefaultNavigator = StackNavigator(routeConfigMap, stackConfig);
+// const AppNavigator = Platform.OS === 'web' ?
+//   BrowserAppContainer(DefaultNavigator) : DefaultNavigator;
+
+export default DefaultNavigator;
